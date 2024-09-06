0c3ba6699e6d5e4b3a612212ed116e43f71c0809
Roshni <roshni.mali@bridgelabz.com>
2024-09-06 16:09:12 +0530

    message

---
+++ logic-practice/src/main/java/com/bridgelabz/logicpractice/AnagramChecker.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.logicpractice;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ logic-practice/src/main/java/com/bridgelabz/logicpractice/FibonacciSeries.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.logicpractice;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println("Fibonacci Series");
+    }
+}
