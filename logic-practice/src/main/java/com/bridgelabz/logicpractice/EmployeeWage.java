ff46b8d2ccda9e2ec598c42ee8dfca712781b0b6
Roshni <roshni.mali@bridgelabz.com>
2024-09-10 11:22:56 +0530

    Modified: to calculate daily employee wage

--- logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
+++ logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
@@ -4,13 +4,18 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
         int IS_PRESENT = 1;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOURS = 8;
 
         // UC1: Check if the employee is present or absent
-        double attendanceCheck = Math.random(); // generates a random value between 0.0 and 1.0
-        int attendance = (attendanceCheck < 0.5) ? 0 : 1; // simulating 50% attendance
+        double attendanceCheck = Math.random();
+        int attendance = (attendanceCheck < 0.5) ? 0 : 1;
 
         if (attendance == IS_PRESENT) {
             System.out.println("Employee is Present");
+            // UC2: Calculate Daily Employee Wage
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+            System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
         }
