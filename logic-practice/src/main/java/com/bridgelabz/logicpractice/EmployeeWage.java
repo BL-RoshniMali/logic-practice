699c35b88b334b2713f56e05a3f7cd5b938b887b
Roshni <roshni.mali@bridgelabz.com>
2024-09-10 11:44:36 +0530

    Refactor: Calculate Wages till the Condition of Total Working Hours or Days is Reached

--- logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
+++ logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
@@ -7,33 +7,40 @@ public class EmployeeWage {
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOURS = 8;
     public static final int PART_TIME_HOURS = 5;
-    public static final int WORKING_DAYS_PER_MONTH = 25;
+    public static final int MAX_WORKING_HOURS = 100;
+    public static final int MAX_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
         int totalWage = 0;
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+        while (totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+            totalWorkingDays++;
             int empType = (int) (Math.random() * 3); // 0 for Absent, 1 for Full-time, 2 for Part-time
             int workingHours = 0;
 
             switch (empType) {
                 case FULL_TIME:
                     workingHours = FULL_DAY_HOURS;
-                    System.out.println("Day " + day + ": Full-time Employee");
+                    System.out.println("Day " + totalWorkingDays + ": Full-time Employee");
                     break;
                 case PART_TIME:
                     workingHours = PART_TIME_HOURS;
-                    System.out.println("Day " + day + ": Part-time Employee");
+                    System.out.println("Day " + totalWorkingDays + ": Part-time Employee");
                     break;
                 default:
-                    System.out.println("Day " + day + ": Employee Absent");
+                    System.out.println("Day " + totalWorkingDays + ": Employee Absent");
                     break;
             }
 
+            totalWorkingHours += workingHours;
             int dailyWage = WAGE_PER_HOUR * workingHours;
             totalWage += dailyWage;
         }
 
         System.out.println("Total Wage for the Month: " + totalWage);
+        System.out.println("Total Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalWorkingHours);
     }
 }
