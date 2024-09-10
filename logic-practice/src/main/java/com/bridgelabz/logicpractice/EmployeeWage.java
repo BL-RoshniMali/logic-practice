d8be0801c35164f3a99e2b51fecb08c15a61fe8d
Roshni <roshni.mali@bridgelabz.com>
2024-09-10 11:39:19 +0530

    Refactor: Solving using switch case statement

--- logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
+++ logic-practice/src/main/java/com/bridgelabz/logicpractice/EmployeeWage.java
@@ -2,28 +2,31 @@ package com.bridgelabz.logicpractice;
 
 public class EmployeeWage {
 
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 5;
+
     public static void main(String[] args) {
-        int IS_PRESENT = 1;
-        int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOURS = 8;
-        int PART_TIME_HOURS = 5;
+        int empType = (int) (Math.random() * 3); // Generates a random value of 0, 1, or 2
 
-        // UC1: Check if the employee is present or absent
-        double attendanceCheck = Math.random();
-        int attendance = (attendanceCheck < 0.5) ? 0 : 1;
-
-        if (attendance == IS_PRESENT) {
-            System.out.println("Employee is Present");
-
-            // UC3: Part-time employee condition
-            boolean isPartTime = Math.random() < 0.5; // Randomly decide part-time or full-time
-            int workingHours = isPartTime ? PART_TIME_HOURS : FULL_DAY_HOURS;
-
-            // UC2: Calculate Daily Employee Wage
-            int dailyWage = WAGE_PER_HOUR * workingHours;
-            System.out.println("Daily Employee Wage: " + dailyWage);
-        } else {
-            System.out.println("Employee is Absent");
+        int workingHours = 0;
+        switch (empType) {
+            case FULL_TIME:
+                workingHours = FULL_DAY_HOURS;
+                System.out.println("Employee is Full-time");
+                break;
+            case PART_TIME:
+                workingHours = PART_TIME_HOURS;
+                System.out.println("Employee is Part-time");
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
+
+        int dailyWage = WAGE_PER_HOUR * workingHours;
+        System.out.println("Daily Employee Wage: " + dailyWage);
     }
 }
